2e0a573a5d64c5c0c6c6246fd210c9bfdabde373
MarcoWilliams <marco@playfab.com>
2015-11-25 12:55:45 -0800

    Testing 3rd commit.

--- MainCloudScript.js
+++ MainCloudScript.js
@@ -2,8 +2,11 @@ var handlers ={
 	helloWorld: funciton(){
 		return "hello world";
 	},
+
 	testMethod: function(){
 		return {"message":"Hello World"}
-	}
+	},
 
+	anotherMethod: function(){
+	}
 }

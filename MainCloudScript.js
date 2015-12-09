c3e5e28048a0490e1ab316c5341f2618d72126b6
MarcoWilliams <marco@playfab.com>
2015-12-09 14:31:32 -0800

    test

--- MainCloudScript.js
+++ MainCloudScript.js
@@ -1,7 +1,7 @@
 var handlers = {
     HelloWorld: function () {
         var result = {};
-        result.message = 'hello world 6';
+        result.message = 'hello world 8';
         return result;
     }
 };

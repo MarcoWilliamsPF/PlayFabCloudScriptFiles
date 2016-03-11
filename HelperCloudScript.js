a81ad4493e5274eee780e6523b54a62c0c2dcfdc
MarcoWilliams <marco@playfab.com>
2016-03-11 12:40:29 -0800

    Test Branch

--- HelperCloudScript.js
+++ HelperCloudScript.js
@@ -9,5 +9,12 @@ var helpers = {
 		var result = {};
 		result.message = "Test Method 3";
 		return result;
+	},
+
+    TestMethod: function(){
+		var result = {};
+		result.message = "Branch Test Method";
+		return result;
 	}
+
 };
